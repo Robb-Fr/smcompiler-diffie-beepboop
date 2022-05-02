4613c347573edacfeffa27f99c2ccb03961392a5
manonmichel <manon.michel.iphone@gmail.com>
2022-05-02 11:34:24 +0200

    implemented secret shares

--- smcompiler/secret_sharing.py
+++ smcompiler/secret_sharing.py
@@ -3,6 +3,7 @@ Secret sharing scheme.
 """
 
 from typing import List
+import random as rd
 
 
 class Share:
@@ -30,12 +31,26 @@ class Share:
 
 def share_secret(secret: int, num_shares: int) -> List[Share]:
     """Generate secret shares."""
-    raise NotImplementedError("You need to implement this method.")
+    q = 23099 # TODO: Change this lol
+
+    shares = rd.sample(range(0,q),num_shares)
+
+    share_0 = secret - (sum(shares) mod % q)
+
+    shares.insert(0,share_0)
+
+    return shares
 
 
 def reconstruct_secret(shares: List[Share]) -> int:
     """Reconstruct the secret from shares."""
     raise NotImplementedError("You need to implement this method.")
+
+
+def gen_prime(min: int, max: int) -> int:
+    n = randint(min, max)
+    while not isPrime(n):
+        n = randint(min , max)
 
 
 # Feel free to add as many methods as you want.
