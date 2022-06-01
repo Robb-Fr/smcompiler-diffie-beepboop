ec5051a13070f901a2ba1d39d47541e31a6396c3
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-06-01 21:01:39 +0200

    gneugneu 1 core

--- run_benchmark_manon.sh
+++ run_benchmark_manon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-pytest -k "parties" -v --benchmark-save="parties_2cores_x86_64" --benchmark-histogram="parties_2cores_x86_64" --benchmark-min-rounds=25
-pytest -k "sec_add" -v --benchmark-save="sec_add_2cores_x86_64" --benchmark-histogram="sec_add_2cores_x86_64" --benchmark-min-rounds=25
-pytest -k "scal_add" -v --benchmark-save="scal_add_2cores_x86_64" --benchmark-histogram="scal_add_2cores_x86_64" --benchmark-min-rounds=25
-pytest -k "sec_mul" -v --benchmark-save="sec_mul_2cores_x86_64" --benchmark-histogram="sec_mul_2cores_x86_64" --benchmark-min-rounds=25
-pytest -k "scal_mul" -v --benchmark-save="scal_mul_2cores_x86_64" --benchmark-histogram="scal_mul_2cores_x86_64" --benchmark-min-rounds=25
+pytest -k "parties" -v --benchmark-save="parties_1core_x86_64" --benchmark-histogram="parties_1core_x86_64" --benchmark-min-rounds=25
+pytest -k "sec_add" -v --benchmark-save="sec_add_1core_x86_64" --benchmark-histogram="sec_add_1core_x86_64" --benchmark-min-rounds=25
+pytest -k "scal_add" -v --benchmark-save="scal_add_1core_x86_64" --benchmark-histogram="scal_add_1core_x86_64" --benchmark-min-rounds=25
+pytest -k "sec_mul" -v --benchmark-save="sec_mul_1core_x86_64" --benchmark-histogram="sec_mul_1core_x86_64" --benchmark-min-rounds=25
+pytest -k "scal_mul" -v --benchmark-save="scal_mul_1core_x86_64" --benchmark-histogram="scal_mul_1core_x86_64" --benchmark-min-rounds=25
