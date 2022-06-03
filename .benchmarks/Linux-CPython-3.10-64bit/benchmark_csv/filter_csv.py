50511c8490d42a73aeed3035beee926a9b8cd0ff
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-06-03 12:47:38 +0200

    moves csv ot benchmarks folder

--- .benchmarks/Linux-CPython-3.10-64bit/benchmark_csv/filter_csv.py
+++ .benchmarks/Linux-CPython-3.10-64bit/benchmark_csv/filter_csv.py
@@ -4,20 +4,22 @@ from sys import argv
 if __name__ == "__main__":
     id = argv[1]
     new_csv = []
-    with open(f"{id}.csv", "r") as f:
+    with open(f"{id}_8cores_arm64.csv", "r") as f:
         reader = csv.reader(f)
-        new_csv.append(reader.__next__())
+        headers = reader.__next__()
+        headers[0] = f"# {id}"
+        new_csv.append(headers)
         for row in reader:
             new_row = []
             for i, col in enumerate(row):
                 if i == 0:
-                    new_row.append(col[38:])
+                    new_row.append(col.split("_")[-1])
                 if i > 0 and i < 6:
                     new_row.append("{:.3f}".format(float(col)))
                 if i == 6:
                     new_row.append(col)
             print(new_row)
             new_csv.append(new_row)
-    with open(f"new_{id}.csv", "w") as f:
+    with open(f"new_{id}_8cores_arm64.csv", "w") as f:
         writer = csv.writer(f)
         writer.writerows(new_csv)
